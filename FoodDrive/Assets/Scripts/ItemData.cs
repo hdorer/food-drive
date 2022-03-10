d2e2fbfe34f141634f91e5dc5ea2d969777de6f2
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-03-10 18:27:14 -0500

    Set up budget system

--- FoodDrive/Assets/Scripts/ItemData.cs
+++ FoodDrive/Assets/Scripts/ItemData.cs
@@ -14,10 +14,10 @@ public struct ItemData {
     // Static array with data for each type of item.  Each ShelfItem will have an ItemData field which grabs data from here.
     // This is a variation on a pattern I saw in Java once, but C# is basically Java anyway.
     public static ItemData[] ITEM_DATA_LIST = new ItemData[(int)ItemType.NUM_ITEMS] {
-        new ItemData(0, ItemType.PURPLE_SQUARE),
-        new ItemData(0, ItemType.ORANGE_CIRCLE),
-        new ItemData(0, ItemType.GREEN_CAPSULE),
-        new ItemData(0, ItemType.RED_CIRCLE),
-        new ItemData(0, ItemType.ORANGE_SQUARE)
+        new ItemData(1, ItemType.PURPLE_SQUARE),
+        new ItemData(2, ItemType.ORANGE_CIRCLE),
+        new ItemData(3, ItemType.GREEN_CAPSULE),
+        new ItemData(4, ItemType.RED_CIRCLE),
+        new ItemData(5, ItemType.ORANGE_SQUARE)
     };
 }
