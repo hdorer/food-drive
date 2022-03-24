58817f56e4f6178a0233124b22f49498b62c3256
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-03-24 18:11:19 -0400

    Finished ItemDataList

--- FoodDrive/Assets/Scripts/GameManager.cs
+++ FoodDrive/Assets/Scripts/GameManager.cs
@@ -15,11 +15,33 @@ public class GameManager : MonoBehaviour {
         instance = this;
 
         itemDataList = new ItemData[(int)ItemType.NUM_ITEMS] {
-            new ItemData(false, true, false, 3),
-            new ItemData(false, false, true, 2),
-            new ItemData(false, false, true, 5),
-            new ItemData(false, false, true, 2),
-            new ItemData(true, true, false, 2)
+            new ItemData(false, true, false, 3), // APPLESAUCE
+            new ItemData(false, false, true, 2), // CANNED_BEANS
+            new ItemData(false, false, true, 5), // CANNED_CHICKEN 
+            new ItemData(false, true, false, 5), // CANNED_FISH
+            new ItemData(false, false, true, 4), // CANNED_MEAT
+            new ItemData(false, false, true, 2), // CANNED_VEGETABLES
+            new ItemData(false, false, true, 4), // COOKING_OIL
+            new ItemData(false, true, false, 4), // CRACKERS
+            new ItemData(false, true, true, 4), // DRIED_HERBS_SPICES
+            new ItemData(true, true, false, 2), // CANNED_FRUITS
+            new ItemData(true, true, false, 3), // GRANOLA_BARS
+            new ItemData(false, false, true, 2), // INSTANT_MASHED_POTATOES
+            new ItemData(true, true, true, 1), // INSTANT_RAMEN
+            new ItemData(true, true, false, 3), // NUTS
+            new ItemData(false, false, true, 2), // PASTA
+            new ItemData(true, true, false, 5), // PEANUT_BUTTER
+            new ItemData(false, true, true, 3), // RICE
+            new ItemData(true, true, true, 4), // POWDERED_MILK
+            new ItemData(false, true, true, 4), // SOUP_STEW_CHILI
+            new ItemData(true, false, false, 5), // WHOLE_GRAIN_CEREAL
+            new ItemData(false, false, false, 5), // GLASS_JAR
+            new ItemData(false, false, false, 5), // FRESH_CHEESE
+            new ItemData(false, false, false, 4), // FRESH_MILK
+            new ItemData(false, false, false, 10), // BULK_RICE
+            new ItemData(false, false, false, 4), // CANDY
+            new ItemData(false, false, false, 12), // RAW_PRODUCE
+            new ItemData(false, false, false, 8), // SUGARY_DRINKS
         };
     }
 }
