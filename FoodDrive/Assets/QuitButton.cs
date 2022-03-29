db974ed055b22d30269f7d989768b789690046d8
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-03-29 00:27:39 -0400

    Play and quit button functionality added

---
+++ FoodDrive/Assets/QuitButton.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+    void OnMouseDown() {
+        Application.Quit();
+    }
+}
